42a4053b8fbbbeb6ed7e7b6614eaa7c2742e6f0d
jeff <jeffli.1644@gmail.com>
2021-06-07 11:07:43 -0400

    using monospace alias

--- config.h
+++ config.h
@@ -17,8 +17,8 @@ static int swallowfloating    = 1;        /* 1 means swallow floating windows by
 static int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static int showbar            = 1;        /* 0 means no bar */
 static int topbar             = 1;        /* 0 means bottom bar */
-static char *fonts[]          = { 
-	"JetBrains Mono Nerd Font:size=11", 
+static char *fonts[]          = {
+	"monospace:size=11",
 };
 static char normbgcolor[]           = "#222222";
 static char normbordercolor[]       = "#444444";
